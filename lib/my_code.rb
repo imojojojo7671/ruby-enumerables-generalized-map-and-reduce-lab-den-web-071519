b541070fd70c17c38eb78de1f5d1176a6c2135b2
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-09 01:01:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,11 +1,12 @@
 def map(source_array)
   map_array = []
-  source_array.length.times { |index|
-    map_array << yield
+  source_array.each { |index|
+    map_array << yield(index)
   }
   map_array
 end
 
-map(source_array[index]) do |n|
-  source_array[index] * -1
-end
+#map(source_array[index]) do |n|
+#  source_array[index] * -1
+#end
+
